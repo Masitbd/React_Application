a87dfa56ca6d863dc53559dd3edc3166da6e8454
Murad Sarker <sarker24@gmail.com>
2021-05-16 23:36:24 +0200

    receive Api data use asios

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -2,16 +2,34 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class CountryList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mydata: [],
+    };
+  }
+
   componentDidMount() {
     axios
       .get("https://restcountries.eu/rest/v2/all")
       .then((response) => {
-        console.log(response.data);
+        this.setState({ mydata: response.data });
       })
       .catch((error) => {});
   }
   render() {
-    return <div></div>;
+    const myList = this.state.mydata;
+    const countryName = myList.map((data) => {
+      return <li>{data.name}</li>;
+    });
+    return (
+      <div className="container">
+        <ul>
+          <li>{countryName}</li>
+        </ul>
+      </div>
+    );
   }
 }
 
