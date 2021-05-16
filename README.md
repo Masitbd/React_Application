3c8a03a4b3829aad0f1c6aa3915ea0c21cf0f906
Murad Sarker <sarker24@gmail.com>
2021-05-16 21:38:05 +0200

    Add the sass file

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 ## Usee state
 
 ## Use functional and class
+
+## Fix the scss problem
+
+- npm uninstall node-sass
+- npm install node-sass@4.14.1
+  Or, if using yarn (default in newer CRA versions)
+
+- yarn remove node-sass
+- yarn add node-sass@4.14.1
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../App.css";
 import styles from "../components/Mycss.module.css";
+import "../components/My.scss";
 
 class About extends Component {
   render() {
@@ -22,6 +23,17 @@ class About extends Component {
           commonly used to demonstrate the visual form of a document or a
           typeface without relying on meaningful content. Lorem ipsum may be
           used as a placeholder before final copy is available.
+        </p>
+        <p className="textstyle">
+          If there is a constructor() function in your component, this function
+          will be called when the component gets initiated. The constructor
+          function is where you initiate the component's properties. In React,
+          component properties should be kept in an object called state. You
+          will learn more about state later in this tutorial. The constructor
+          function is also where you honor the inheritance of the parent
+          component by including the super() statement, which executes the
+          parent component's constructor function, and your component has access
+          to all the functions of the parent component (React.Component).
         </p>
       </div>
     );
